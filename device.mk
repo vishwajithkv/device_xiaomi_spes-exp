bb99b38569d499fea3850433aeb67aa98a82ce03
boedhack99 <boedhack@gmail.com>
2023-02-04 20:23:45 +0700

    spes: Build sensor multihal from source
    Change-Id: Idde718a2c98375ee96be73c2597088417ccde864

--- device.mk
+++ device.mk
@@ -410,7 +410,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.spes-multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
